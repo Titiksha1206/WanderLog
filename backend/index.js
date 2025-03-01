b5fc4ec77130b6be69f2bbcf9066731ffe37e6a2
sajalsingh2115 <sajal.singh2105@gmail.com>
2025-03-01 21:52:37 +0530

    login api

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,6 @@ import jwt from 'jsonwebtoken';
 import mongoose from 'mongoose';
 
 import 'dotenv/config';
-// import config from './config.json' assert { type: 'json' };
 
 import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
@@ -16,11 +15,11 @@ import { User } from './models/user.model.js';
 mongoose.connect(config.connectionstring);
 
 const app = express();
-// app.get('/', (req, res) => {
-//     res.send('Hello World!');
-// });
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
 
-app.use(express.json({limit: 16000}));
+app.use(express.json()); // middleware to parse JSON
 
 app.use(cors(
     {
@@ -66,6 +65,35 @@ app.post("/create-account", async (req, res) => {
     return res.status(201).json({ error: false, user: {username: user.username, email: user.email}, accessToken,
         message: 'Registration Successfully'
      });
+});
+
+// create LOGIN API
+app.post("/login", async (req, res) => {
+    const {email, password} = req.body;
+
+    if(!email ||  !password) {
+        return res.status(400).json({error: true, message: 'ALL FIELDS ARE REQUIRED'});
+    }
+    const user = await User.findOne({email});
+    if(!user) {
+        return res.status(400).json({error: true, message: 'Invalid credentials'});
+    }
+
+    const isValidPassword = await bcrypt.compare(password, user.password);
+    if(!isValidPassword) {
+        return res.status(400).json({error: true, message: 'Invalid credentials'});
+    
+    }
+        const accessToken = jwt.sign(
+            { userId: user._id },
+            process.env.ACCESS_TOKEN_SECRET,
+            {expiresIn:"72h"}
+        );
+        return res.status(400).json(
+            { 
+            error: false, 
+            user: {username: user.username, email: user.email}, accessToken, message: 'Login Successful'
+            });
 });
 
 
