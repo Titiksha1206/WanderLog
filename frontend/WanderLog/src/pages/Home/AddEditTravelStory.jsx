472677f89928ad6328e297e5c64c3bfb92efe044
1Megha106 <gmegha375@gmail.com>
2025-04-26 17:42:40 +0530

    Update AddEditTravelStory.jsx

--- frontend/WanderLog/src/pages/Home/AddEditTravelStory.jsx
+++ frontend/WanderLog/src/pages/Home/AddEditTravelStory.jsx
@@ -7,6 +7,7 @@ const AddEditTravelStory = ({
     onClose,
     getAllTravelStories, 
 }) => {
+    const handleAddOrUpdateClick = () => {};
         return  
         <div>
             <div className="flex items-center justify-between">  
@@ -16,16 +17,36 @@ const AddEditTravelStory = ({
 
                 <div>
                     <div className="flex items-center gap-3 bg-cyan-50/50 p-2 rounded-l-lg">
-                    <button className="btn-small" onCLick={()=>{}}>
+                        {type === 'add' ? <button className="btn-small" onCLick={()=>{}}>
                       <MdAdd className="text-lg" /> ADD STORY
+                    </button> : <>
+                    <button className="btn-small" onClick={handleAddOrUpdateClick}>
+                       <MdUpdate className="text-lg" /> UPDATE STORY
                     </button>
+
+                       /* <button className ="btn-small btn-delete" onClick={onClose}>
+                            <MdDeleteOutline className="text-lg" /> DELETE 
+                        </button> */
+                    </>}
 
                     <button className="" onCLick={onCLose}>
                       <MdAdd className="text-xl text-slate-400" /> 
                     </button>
                 </div>
-            </div>    
+            </div>  
+                <div className = "flex-1 flex flex-col gap-2 pt-4">
+                <label className="input-label"> TITLE </label>
+                <input 
+                    type="text"
+                    className="text-2xl text-slate-950 outline-none"
+                    placeholder=" A Day at the Great Wall"
+                    />
+                    <div className="my-3">
+                    <DateSelector/>
+                    </div>
+                </div>
         </div>;
+    };
     };
 
     export default AddEditTravelStory;
