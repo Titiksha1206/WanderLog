2f519442066f52dfb8679192722cfc0ae525ce40
1Megha106 <gmegha375@gmail.com>
2025-04-26 18:52:05 +0530

    Update AddEditTravelStory.jsx

--- frontend/WanderLog/src/pages/Home/AddEditTravelStory.jsx
+++ frontend/WanderLog/src/pages/Home/AddEditTravelStory.jsx
@@ -7,6 +7,13 @@ const AddEditTravelStory = ({
     onClose,
     getAllTravelStories, 
 }) => {
+
+    const [title,setTitle] = useState("");
+    const [storyImg, setStoryImg] = UseState(null);
+    const [story, setStory] = useState("");
+    const [visitedLocation, setVisitedLocation] = useState(null);
+    const [visitedDate, setVisitedDate] = useState(null);
+    
     const handleAddOrUpdateClick = () => {};
         return  
         <div>
@@ -40,9 +47,27 @@ const AddEditTravelStory = ({
                     type="text"
                     className="text-2xl text-slate-950 outline-none"
                     placeholder=" A Day at the Great Wall"
+                    value={title}
+                    onChange={({ target }) => setTitle(target.value)}
                     />
+                    
                     <div className="my-3">
-                    <DateSelector/>
+                    <DateSelector date= {visitedDate} setDate={setvisitedDate} />
+                      </div>
+
+                    <ImageSelector image={storyImg} setImage={setStoryImg} />
+                        
+                        <div className="flex flex-col gap-2 mt-4">
+                        <label classsName="input-label">STORY</label>
+                        <textarea
+                            type="text"
+                            className="text-sm text-slate-950 outline-nonne bg-slate-50 pt-2 rounded"
+                            placeholder="Your Story"
+                            rows={10}
+                            value={story}
+                            onChange={({ target }) => setStory(target.value)}
+                            />
+                        </div>
                     </div>
                 </div>
         </div>;
