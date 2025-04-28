b1616184ce6e7a22ac252ff06ed9b030b095638d
Titiksha1206 <titiksha1206@gmail.com>
2025-04-28 21:14:27 +0530

    logoupdate

--- frontend/WanderLog/src/components/Navbar.jsx
+++ frontend/WanderLog/src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import LOGO from "../assets/images/LOGO.jpeg";
+import LOGO from "../assets/images/LOGO.png";
 import ProfileInfo from "./Cards/ProfileInfo";
 import { useNavigate } from "react-router-dom";
 
@@ -15,7 +15,7 @@ const Navbar = ({userInfo}) => {
     
     return (
         <div className='bg-white flex items-center justify-between px-6 py-2 drop-shadow sticky top-0 z-10'>
-            <img src={LOGO} alt="travel story" classmate="h-9" width={190} />      
+            <img src={LOGO} alt="travel story" classmate="h-9" width={70} />      
             
            {isToken && <ProfileInfo userInfo={userInfo} onLogout={onLogout}/>}
        </div>
