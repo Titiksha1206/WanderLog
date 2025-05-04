34f0bf7210831ac3081e5e539a267712566dd1e9
Titiksha1206 <titiksha1206@gmail.com>
2025-05-04 20:58:32 +0530

    logo navigate

--- frontend/WanderLog/src/components/Navbar.jsx
+++ frontend/WanderLog/src/components/Navbar.jsx
@@ -4,47 +4,61 @@ import ProfileInfo from "./Cards/ProfileInfo";
 import { useNavigate } from "react-router-dom";
 import SearchBar from "./Input/SearchBar";
 
-const Navbar = ({userInfo, searchQuery, setSearchQuery, onSearchNote, handleClearSearch}) => {
+const Navbar = ({
+  userInfo,
+  searchQuery,
+  setSearchQuery,
+  onSearchNote,
+  handleClearSearch,
+}) => {
+  const isToken = localStorage.getItem("token");
+  const navigate = useNavigate();
 
-    const isToken = localStorage.getItem("token");
-    const navigate = useNavigate();   
+  const onLogout = () => {
+    localStorage.clear();
+    navigate("/login");
+  };
 
-    const onLogout = ()=>{
-        localStorage.clear();
-        navigate("/login");
-    };
-    
-    const handleSearch = ()=>{
-        if(searchQuery){
-            onSearchNote(searchQuery);
-        }
-    };
+  const handleSearch = () => {
+    if (searchQuery) {
+      onSearchNote(searchQuery);
+    }
+  };
 
-    const onClearSearch = ()=>{
-        handleClearSearch();
-        setSearchQuery("");
-    };
+  const onClearSearch = () => {
+    handleClearSearch();
+    setSearchQuery("");
+  };
 
-    return (
-        <div className='bg-white flex items-center justify-between px-6 py-2 drop-shadow sticky top-0 z-10'>
-            <img src={LOGO} alt="travel story" classmate="h-9" width={70} />      
-            
-           {isToken && (
-             <>
-             <SearchBar
-             value = {searchQuery}
-             onChange = {({target}) =>{
-                setSearchQuery(target.value);
-             }}
-             handleSearch = {handleSearch}
-             onClearSearch = {onClearSearch}
-             />
+  return (
+    <div className="bg-white flex items-center justify-between px-6 py-2 drop-shadow sticky top-0 z-10">
+      <div
+        onClick={() => {
+          setSearchQuery(""); // Clear the search input
+          handleClearSearch(); // Fetch all travel stories
+          navigate("/dashboard"); // Navigate to dashboard
+        }}
+        className="cursor-pointer"
+      >
+        <img src={LOGO} alt="travel story" className="h-9" width={70} />
+      </div>
 
-               <ProfileInfo userInfo={userInfo} onLogout={onLogout}/> 
-             </>
-            )}
-       </div>
-    )
-}
+      {isToken && (
+        <>
+          <SearchBar
+            value={searchQuery}
+            onChange={({ target }) => {
+              setSearchQuery(target.value);
+            }}
+            handleSearch={handleSearch}
+            onClearSearch={onClearSearch}
+          />
 
-export default Navbar
+          <ProfileInfo userInfo={userInfo} onLogout={onLogout} />
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Navbar;
